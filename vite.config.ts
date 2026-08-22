f80d4ff9ef7cbfd0bd58388dbdf8139a8f1049d1
Sameer <sameervfx1@gmail.com>
2026-08-21 19:15:31 -0700

    fix(vercel): move vite build tools to dependencies so Vercel installs vite on production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: './',
+  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
